2a7870d2c006df0b83127754343d739b71507568
Chris <chrixs99@hotmail.com>
2017-01-23 13:11:10 +0000

    statement initializes with an empty transactions array

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -11,4 +11,8 @@ describe Statement do
     expect(new_statement).to be_an_instance_of Statement
   end
 
+  it "expects a statement to intialize with an empty array" do
+    expect(statement.transactions).to be_instance_of Array
+  end
+
 end
